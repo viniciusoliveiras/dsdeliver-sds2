2eb31fa841254d325dbe4fdd690a40b62df7ed2d
viniciusoliveiras <vinitag190@gmail.com>
2021-01-10 17:55:10 -0300

    Completion of the project

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,28 +1,69 @@
-import React from "react";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, ScrollView, Alert } from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
+import { fetchOrders } from "../api";
 import Header from "../Header";
-import OrderCard from '../OrderCard'
+import OrderCard from "../OrderCard";
+import { Order } from "../types";
+import { useIsFocused, useNavigation } from "@react-navigation/native";
 
 function Orders() {
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigation = useNavigation();
+  const isFocused = useIsFocused()
+  const fetchData = () => {
+    setIsLoading(true);
+    fetchOrders()
+      .then((response) => setOrders(response.data))
+      .catch(() => Alert.alert("Erro ao carregar os pedidos"))
+      .finally(() => setIsLoading(false));
+  };
+
+  useEffect(() => {
+    if (isFocused) {
+      fetchData();
+    }
+  }, [isFocused]);
+
+  const handleOnPress = (order: Order) => {
+    navigation.navigate("OrderDetails", {
+      order,
+    });
+  };
+
   return (
     <>
       <Header />
       <ScrollView style={styles.container}>
-        <OrderCard/>
-        <OrderCard/>
-        <OrderCard/>
-        <OrderCard/>
-        <OrderCard/>
-        <OrderCard/>
+        {isLoading ? (
+          <Text style={styles.searchOrders}>Buscando pedidos . . .</Text>
+        ) : (
+          orders.map((order) => (
+            <TouchableWithoutFeedback
+              key={order.id}
+              onPress={() => handleOnPress(order)}
+            >
+              <OrderCard order={order} />
+            </TouchableWithoutFeedback>
+          ))
+        )}
       </ScrollView>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    paddingRight: '5%',
-    paddingLeft: '5%'
+  container: {
+    paddingRight: "5%",
+    paddingLeft: "5%",
+  },
+  searchOrders:{
+    color: "#9E9E9E",
+    fontSize: 30,
+    marginTop: 150,
+    lineHeight: 30,
+    textAlign: "center",
   }
 });
 
--- front-mobile/src/Routes.tsx
+++ front-mobile/src/Routes.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./Home";
 import Orders from "./Orders";
+import OrderDetails from "./OrderDetails";
 
 const Stack = createStackNavigator();
 
@@ -13,8 +14,9 @@ function Routes() {
         headerMode="none"
         screenOptions={{ cardStyle: { backgroundColor: "#FFF" } }}
       >
-          <Stack.Screen name='Home' component={Home}></Stack.Screen>
-          <Stack.Screen name='Orders' component={Orders}></Stack.Screen>
+        <Stack.Screen name="Home" component={Home}></Stack.Screen>
+        <Stack.Screen name="Orders" component={Orders}></Stack.Screen>
+        <Stack.Screen name="OrderDetails" component={OrderDetails}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
